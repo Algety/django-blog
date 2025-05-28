6ea12e98bf47bd454b7198b1cd8a2d5e547eb529
Algety <algety@ukr.net>
2025-05-28 18:47:42 +0300

    Template added

--- blog/admin.py
+++ blog/admin.py
@@ -1,5 +1,12 @@
 from django.contrib import admin
-from .models import Post
+from .models import Post, Comment
 
 # Register your models here.
-admin.site.register(Post)
+
+@admin.register(Post)
+class PostAdmin(admin.ModelAdmin):
+    list_display = ('title', 'author')
+    search_fields = ('title',)
+
+# admin.site.register(Post)
+admin.site.register(Comment)
