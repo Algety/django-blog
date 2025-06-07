5f3542941580be476527c638bafc7397b7c0c13d
Algety <algety@ukr.net>
2025-06-08 02:55:18 +0300

    Edit and Delete buttons added

--- about/views.py
+++ about/views.py
@@ -1,12 +1,23 @@
 from django.shortcuts import render, get_object_or_404
 from .models import About, CollaborateRequest
 from .forms import CollaborateForm
+from django.contrib import messages
 
 # Create your views here.
 
 
 def about_me(request):
     about = About.objects.order_by("updated").first()
+
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data=request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            messages.add_message(
+                request, messages.SUCCESS,
+                'Collaboration request received! I endeavour to respond within 2 working days.'
+    )
+
     collaborate_form = CollaborateForm()
     # about = get_object_or_404(queryset, about=about)
 
@@ -17,3 +28,25 @@ def about_me(request):
          "collaborate_form": collaborate_form
         },
     )
+
+def comment_edit(request):
+    """
+    view to edit comments
+    """
+    if request.method == "POST":
+
+        queryset = Post.objects.filter(status=1)
+        post = get_object_or_404(queryset, slug=slug)
+        comment = get_object_or_404(Comment, pk=comment_id)
+        comment_form = CommentForm(data=request.POST, instance=comment)
+
+        if comment_form.is_valid() and comment.author == request.user:
+            comment = comment_form.save(commit=False)
+            comment.post = post
+            comment.approved = False
+            comment.save()
+            messages.add_message(request, messages.SUCCESS, 'Comment Updated!')
+        else:
+            messages.add_message(request, messages.ERROR, 'Error updating comment!')
+
+    return HttpResponseRedirect(reverse('post_detail', args=[slug]))
--- static/js/script.js
+++
@@ -1,2 +0,0 @@
-// print("Hello Django")
-console.log("Hello Django console")
